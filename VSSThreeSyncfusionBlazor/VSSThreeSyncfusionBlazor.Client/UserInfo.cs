bda249c83c1abff83dc50403b73f21b71a13e969
Magnus Mutai <MagnusMq07@gmail.com>
2024-05-15 10:52:29 +0300

    Add project files.

---
+++ VSSThreeSyncfusionBlazor/VSSThreeSyncfusionBlazor.Client/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace VSSThreeSyncfusionBlazor.Client
+{
+    public class UserInfo
+    {
+        public required string UserId { get; set; }
+        public required string Email { get; set; }
+    }
+}
